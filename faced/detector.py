65ce9bc2068d303d10b9a54a2dc14c81e33c7da7
leonmayer <39220957+leonmayer@users.noreply.github.com>
2020-01-11 11:35:15 +0100

    Update detector.py

--- faced/detector.py
+++ faced/detector.py
@@ -11,7 +11,7 @@ class FaceDetector(object):
 
     def __init__(self):
         #self.load_model(os.path.join(MODELS_PATH, "yolov3_irrgb.pb"))
-        self.load_model("/content/gdrive/My Drive/darknetdata/tensorflow/yolov3_irrgb.pb"))
+        self.load_model("/content/gdrive/My Drive/darknetdata/tensorflow/yolov3_irrgb.pb")
         self.load_aux_vars()
 
         self.face_corrector = FaceCorrector()
